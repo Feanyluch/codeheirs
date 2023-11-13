53ea37fdb78c1712059851442f25478528bd3f83
iamfeanyluch <ifeanyioluchi7@gmail.com>
2023-11-14 00:16:55 +0100

    space removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,29 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Navbar from '@/components/Navbar'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Navbar from "@/components/Navbar";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 function Home() {
   return (
     <div>
-    <Navbar />
+      <Navbar />
       <div className="max-w-[1200px] mx-auto py-20 h-[200vh]">
-        <h1 className="text-3xl font-bold">Welcome to My Theme Switcher Loremkd;alkWelcome to My Theme Switcher Loremkd;alkWelcome to My Theme Switcher Loremkd;alkWelcome to My Theme Switcher Loremkd;alkWelcome to My Theme Switcher Loremkd;alkWelcome to My Theme Switcher Loremkd;alkWelcome to My Theme Switcher Loremkd;alkWelcome to My Theme Switcher Loremkd;alkWelcome to My Theme Switcher Loremkd;alkWelcome to My Theme Switcher Loremkd;alkWelcome to My Theme Switcher Loremkd;alkWelcome to My Theme Switcher Loremkd;alkWelcome to My Theme Switcher Loremkd;alkWelcome to My Theme Switcher Loremkd;alkWelcome to My Theme Switcher Loremkd;alk</h1>
+        <h1 className="text-3xl font-bold">
+          Welcome to My Theme Switcher Loremkd;alkWelcome to My Theme Switcher
+          Loremkd;alkWelcome to My Theme Switcher Loremkd;alkWelcome to My Theme
+          Switcher Loremkd;alkWelcome to My Theme Switcher Loremkd;alkWelcome to
+          My Theme Switcher Loremkd;alkWelcome to My Theme Switcher
+          Loremkd;alkWelcome to My Theme Switcher Loremkd;alkWelcome to My Theme
+          Switcher Loremkd;alkWelcome to My Theme Switcher Loremkd;alkWelcome to
+          My Theme Switcher Loremkd;alkWelcome to My Theme Switcher
+          Loremkd;alkWelcome to My Theme Switcher Loremkd;alkWelcome to My Theme
+          Switcher Loremkd;alkWelcome to My Theme Switcher Loremkd;alk
+        </h1>
         <p className="mt-4">Your content goes here.</p>
       </div>
-      </div>
-  )
+    </div>
+  );
 }
 
 export default Home;
